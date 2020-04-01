eac0ceacfb90b3c5e28f45d7e08f10f1e0763292
Petar Nenov <petarnenovpetrov@gmail.com>
2020-04-01 17:25:02 +0300

    add parcel

--- src/__tets__/Edge.test.ts
+++ src/__tets__/Edge.test.ts
@@ -3,10 +3,9 @@ import Vertex from "../Vertex";
 
 describe("Edge class", () => {
   it("should create instance", () => {
-    Vertex.nextUniqueLabel=0;    
+    Vertex.nextUniqueLabel = 0;
     const v1 = new Vertex(0, 0, 5);
     const v2 = new Vertex(100, 0, 2);
-    
 
     const testEdge = new Edge(v1, v2);
   });
